2b063b7b14c7aac9df29d626db5f41e7e234be8d
Дмитрий Щелочков <ddimpalass@icloud.com>
2021-04-08 09:30:27 +0300

    Initial Commit

---
+++ ShchelochowDS_HW3.5/ShchelochowDS_HW3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  ShchelochowDS_HW3_5App.swift
+//  ShchelochowDS_HW3.5
+//
+//  Created by Apple on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShchelochowDS_HW3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
